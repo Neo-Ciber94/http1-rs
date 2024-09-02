4f10fa9cb4943845a29487718d52aa6e80a79932
Freddy L <neo-ciber_94@hotmail.com>
2024-09-02 15:01:22 -0400

    Refactor method

--- src/http/method.rs
+++ src/http/method.rs
@@ -2,30 +2,30 @@ use std::str::FromStr;
 
 #[derive(Debug, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
 pub enum Method {
-    Get,
-    Post,
-    Put,
-    Delete,
-    Patch,
-    Options,
-    Head,
-    Connect,
-    Trace,
+    GET,
+    POST,
+    PUT,
+    DELETE,
+    PATCH,
+    OPTIONS,
+    HEAD,
+    CONNECT,
+    TRACE,
     ExtensionMethod(String),
 }
 
 impl Method {
     pub fn as_str(&self) -> &str {
         match self {
-            Method::Get => "GET",
-            Method::Post => "POST",
-            Method::Put => "PUT",
-            Method::Delete => "DELETE",
-            Method::Patch => "PATCH",
-            Method::Options => "OPTIONS",
-            Method::Head => "HEAD",
-            Method::Connect => "CONNECT",
-            Method::Trace => "TRACE",
+            Method::GET => "GET",
+            Method::POST => "POST",
+            Method::PUT => "PUT",
+            Method::DELETE => "DELETE",
+            Method::PATCH => "PATCH",
+            Method::OPTIONS => "OPTIONS",
+            Method::HEAD => "HEAD",
+            Method::CONNECT => "CONNECT",
+            Method::TRACE => "TRACE",
             Method::ExtensionMethod(ext) => ext.as_str(),
         }
     }
@@ -39,15 +39,15 @@ pub struct InvalidMethod {
 impl<'a> From<&'a str> for Method {
     fn from(value: &'a str) -> Self {
         match value {
-            v if v.eq_ignore_ascii_case("GET") => Method::Get,
-            v if v.eq_ignore_ascii_case("POST") => Method::Post,
-            v if v.eq_ignore_ascii_case("PUT") => Method::Put,
-            v if v.eq_ignore_ascii_case("DELETE") => Method::Delete,
-            v if v.eq_ignore_ascii_case("PATCH") => Method::Patch,
-            v if v.eq_ignore_ascii_case("OPTIONS") => Method::Options,
-            v if v.eq_ignore_ascii_case("HEAD") => Method::Head,
-            v if v.eq_ignore_ascii_case("CONNECT") => Method::Connect,
-            v if v.eq_ignore_ascii_case("TRACE") => Method::Trace,
+            v if v.eq_ignore_ascii_case("GET") => Method::GET,
+            v if v.eq_ignore_ascii_case("POST") => Method::POST,
+            v if v.eq_ignore_ascii_case("PUT") => Method::PUT,
+            v if v.eq_ignore_ascii_case("DELETE") => Method::DELETE,
+            v if v.eq_ignore_ascii_case("PATCH") => Method::PATCH,
+            v if v.eq_ignore_ascii_case("OPTIONS") => Method::OPTIONS,
+            v if v.eq_ignore_ascii_case("HEAD") => Method::HEAD,
+            v if v.eq_ignore_ascii_case("CONNECT") => Method::CONNECT,
+            v if v.eq_ignore_ascii_case("TRACE") => Method::TRACE,
             _ => Method::ExtensionMethod(value.to_string()),
         }
     }
@@ -70,15 +70,15 @@ impl FromStr for Method {
 
     fn from_str(value: &str) -> Result<Self, Self::Err> {
         match value {
-            v if v.eq_ignore_ascii_case("GET") => Ok(Method::Get),
-            v if v.eq_ignore_ascii_case("POST") => Ok(Method::Post),
-            v if v.eq_ignore_ascii_case("PUT") => Ok(Method::Put),
-            v if v.eq_ignore_ascii_case("DELETE") => Ok(Method::Delete),
-            v if v.eq_ignore_ascii_case("PATCH") => Ok(Method::Patch),
-            v if v.eq_ignore_ascii_case("OPTIONS") => Ok(Method::Options),
-            v if v.eq_ignore_ascii_case("HEAD") => Ok(Method::Head),
-            v if v.eq_ignore_ascii_case("CONNECT") => Ok(Method::Connect),
-            v if v.eq_ignore_ascii_case("TRACE") => Ok(Method::Trace),
+            v if v.eq_ignore_ascii_case("GET") => Ok(Method::GET),
+            v if v.eq_ignore_ascii_case("POST") => Ok(Method::POST),
+            v if v.eq_ignore_ascii_case("PUT") => Ok(Method::PUT),
+            v if v.eq_ignore_ascii_case("DELETE") => Ok(Method::DELETE),
+            v if v.eq_ignore_ascii_case("PATCH") => Ok(Method::PATCH),
+            v if v.eq_ignore_ascii_case("OPTIONS") => Ok(Method::OPTIONS),
+            v if v.eq_ignore_ascii_case("HEAD") => Ok(Method::HEAD),
+            v if v.eq_ignore_ascii_case("CONNECT") => Ok(Method::CONNECT),
+            v if v.eq_ignore_ascii_case("TRACE") => Ok(Method::TRACE),
             _ => Err(InvalidMethod { _priv: () }),
         }
     }
