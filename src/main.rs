79ad783a776a752b1c5a40361bd0a9cc3dcb388b
Freddy L <neo-ciber_94@hotmail.com>
2024-09-22 12:41:52 -0400

    refactor sample

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use http1::{
-    body::ChunkedBody,
-    http::{headers, response::Response, status::StatusCode},
+    body::{Body, ChunkedBody},
+    http::{headers, request::Request, response::Response, status::StatusCode},
     server::Server,
 };
 use std::{thread, time::Duration};
@@ -11,24 +11,26 @@ fn main() {
 
     server
         .on_ready(|addr| println!("Listening on http://{addr}"))
-        .listen(|req| {
-            println!("Request: {req:?}");
-            let (chunked, sender) = ChunkedBody::new();
-
-            sender.send(b"<h1>Hello</h1>".to_vec()).ok();
-
-            thread::spawn(move || {
-                thread::sleep(Duration::from_secs(2));
-                sender.send(b"<h2>World</h2>".to_vec()).ok();
-            });
-
-            Response::builder()
-                .insert_header("X-Server", "MyServer")
-                .insert_header(headers::CONTENT_TYPE, "text/html")
-                .insert_header(headers::TRANSFER_ENCODING, "chunked")
-                .insert_header(headers::CONNECTION, "keep-alive")
-                .status(StatusCode::OK)
-                .build(chunked.into())
-        })
+        .listen(handle_request)
         .unwrap();
+}
+
+fn handle_request(req: Request<Body>) -> Response<Body> {
+    println!("Request: {req:?}");
+    let (chunked, sender) = ChunkedBody::new();
+
+    sender.send(b"<h1>Hello</h1>".to_vec()).ok();
+
+    thread::spawn(move || {
+        thread::sleep(Duration::from_secs(2));
+        sender.send(b"<h2>World</h2>".to_vec()).ok();
+    });
+
+    Response::builder()
+        .insert_header("X-Server", "MyServer")
+        .insert_header(headers::CONTENT_TYPE, "text/html")
+        .insert_header(headers::TRANSFER_ENCODING, "chunked")
+        .insert_header(headers::CONNECTION, "keep-alive")
+        .status(StatusCode::OK)
+        .build(chunked.into())
 }
