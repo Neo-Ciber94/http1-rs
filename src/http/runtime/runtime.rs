0133527c5e79f943d76aae9a82e4755a6c005d35
Freddy L <neo-ciber_94@hotmail.com>
2024-09-25 22:28:12 -0400

    docs

--- src/http/runtime/runtime.rs
+++ src/http/runtime/runtime.rs
@@ -7,6 +7,14 @@ pub trait Runtime {
     type Output;
 
     /// Starts running the server an accepting requests.
+    /// 
+    /// To correctly implement a `Runtime` you forward the stream receives from the listener to `http1::protocol::h1::handle_incoming` 
+    /// which manages http1 connections.
+    /// 
+    /// # Parameters
+    /// - `listener`: The socket that receives the connections.
+    /// - `config`: The additional server configuration.
+    /// - `handler`: The request handler.
     fn start<H: RequestHandler + Send + Sync + 'static>(
         self,
         listener: TcpListener,
