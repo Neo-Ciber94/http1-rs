75be7a249da948806cc959a69714d4e140e871d6
Freddy L <neo-ciber_94@hotmail.com>
2024-09-26 20:29:26 -0400

    fix

--- src/version.rs
+++ src/version.rs
@@ -18,7 +18,7 @@ impl Version {
 
 impl Display for Version {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        writeln!(f, "{}", self.as_str())
+        write!(f, "{}", self.as_str())
     }
 }
 
