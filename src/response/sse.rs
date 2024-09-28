8a3c0a2eee35c514bebaeb51bbdd082a9bb256d1
Freddy L <neo-ciber_94@hotmail.com>
2024-09-28 16:01:58 -0400

    Add sse event

--- src/response/sse.rs
+++ src/response/sse.rs
@@ -1,4 +1,50 @@
-pub struct Sse {}
+use std::{
+    fmt::Display,
+    sync::mpsc::{channel, Receiver, Sender},
+};
+
+use crate::body::http_body::HttpBody;
+
+#[derive(Debug)]
+pub struct SseSendError;
+
+pub struct SseStream(Receiver<SseEvent>);
+
+impl SseStream {
+    pub fn new() -> (SseBroadcast, Self) {
+        let (sender, receiver) = channel();
+
+        let sse_broadcast = SseBroadcast(sender);
+        (sse_broadcast, SseStream(receiver))
+    }
+}
+
+#[derive(Debug)]
+pub struct InvalidSseStreamError;
+
+impl HttpBody for SseStream {
+    type Err = InvalidSseStreamError;
+    type Data = Vec<u8>;
+
+    fn read_next(&mut self) -> Result<Option<Self::Data>, Self::Err> {
+        match self.0.recv() {
+            Ok(event) => {
+                let bytes = event.to_string().as_bytes().to_vec();
+                Ok(Some(bytes))
+            }
+            Err(_) => Err(InvalidSseStreamError),
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct SseBroadcast(Sender<SseEvent>);
+
+impl SseBroadcast {
+    pub fn send(&self, event: SseEvent) -> Result<(), SseSendError> {
+        self.0.send(event).map_err(|_| SseSendError)
+    }
+}
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct SseEvent {
@@ -13,14 +59,14 @@ impl SseEvent {
         Builder::new()
     }
 
-    pub fn with_data(data: impl Into<String>) -> Result<Self, InvalidSSeEvent> {
+    pub fn with_data(data: impl Into<String>) -> Result<Self, InvalidSseEvent> {
         Builder::new().data(data)
     }
 
     pub fn with_event_data(
         event: impl Into<String>,
         data: impl Into<String>,
-    ) -> Result<Self, InvalidSSeEvent> {
+    ) -> Result<Self, InvalidSseEvent> {
         Builder::new().event(event).data(data)
     }
 
@@ -41,6 +87,26 @@ impl SseEvent {
     }
 }
 
+impl Display for SseEvent {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if let Some(id) = &self.id {
+            write!(f, "id: {id}\r\n")?;
+        }
+
+        if let Some(event) = &self.event {
+            write!(f, "event: {event}\r\n")?;
+        }
+
+        if let Some(retry) = &self.retry {
+            write!(f, "retry: {retry}\r\n")?;
+        }
+
+        write!(f, "data: {}\r\n", self.data)?;
+
+        Ok(())
+    }
+}
+
 #[derive(Debug, Default)]
 struct Parts {
     id: Option<String>,
@@ -49,21 +115,35 @@ struct Parts {
 }
 
 #[derive(Debug)]
-pub enum InvalidSSeEvent {
-    InvalidId(&'static str),
-    InvalidEvent(&'static str),
-    InvalidData(&'static str),
+pub enum InvalidSseEvent {
+    InvalidIdLineBreak,
+    InvalidEventLineBreak,
+    InvalidDataLineBreak,
+}
+
+impl Display for InvalidSseEvent {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            InvalidSseEvent::InvalidIdLineBreak => write!(f, "'id' cannot contain a line-break"),
+            InvalidSseEvent::InvalidEventLineBreak => {
+                write!(f, "'event' cannot contain a line-break")
+            }
+            InvalidSseEvent::InvalidDataLineBreak => {
+                write!(f, "'data' cannot contain a line-break")
+            }
+        }
+    }
 }
 
 #[derive(Debug)]
-pub struct Builder(Result<Parts, InvalidSSeEvent>);
+pub struct Builder(Result<Parts, InvalidSseEvent>);
 
 impl Builder {
     pub fn new() -> Self {
         Default::default()
     }
 
-    fn update<F: FnOnce(&mut Parts) -> Result<(), InvalidSSeEvent>>(mut self, f: F) -> Self {
+    fn update<F: FnOnce(&mut Parts) -> Result<(), InvalidSseEvent>>(mut self, f: F) -> Self {
         self.0 = self.0.and_then(|mut parts| {
             f(&mut parts)?;
             Ok(parts)
@@ -77,9 +157,7 @@ impl Builder {
 
         self.update(|parts| {
             if has_line_break(&id) {
-                return Err(InvalidSSeEvent::InvalidId(
-                    "'id' cannot contains a line-break",
-                ));
+                return Err(InvalidSseEvent::InvalidIdLineBreak);
             }
 
             parts.id = Some(id);
@@ -92,9 +170,7 @@ impl Builder {
 
         self.update(|parts| {
             if has_line_break(&event) {
-                return Err(InvalidSSeEvent::InvalidEvent(
-                    "'event' cannot contains a line-break",
-                ));
+                return Err(InvalidSseEvent::InvalidEventLineBreak);
             }
 
             parts.event = Some(event);
@@ -109,15 +185,13 @@ impl Builder {
         })
     }
 
-    pub fn data(self, data: impl Into<String>) -> Result<SseEvent, InvalidSSeEvent> {
+    pub fn data(self, data: impl Into<String>) -> Result<SseEvent, InvalidSseEvent> {
         let Parts { event, id, retry } = self.0?;
 
         let data = data.into();
 
         if has_line_break(&data) {
-            return Err(InvalidSSeEvent::InvalidEvent(
-                "'data' cannot contains a line-break",
-            ));
+            return Err(InvalidSseEvent::InvalidDataLineBreak);
         }
 
         Ok(SseEvent {
