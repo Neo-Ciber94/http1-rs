41d24fb3cd71c596634e31f0ccab6c3880d7b71e
Freddy L <neo-ciber_94@hotmail.com>
2024-09-28 17:24:58 -0400

    Test with file

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,8 @@
 use http1::{
-    body::{Body, ChunkedBody},
-    headers,
-    request::Request,
-    response::{
-        sse::{SseEvent, SseStream},
-        Response,
-    },
-    runtime::SingleThreadRuntime,
-    server::Server,
-    status::StatusCode,
+    body::Body, headers, request::Request, response::Response, runtime::SingleThreadRuntime,
+    server::Server, status::StatusCode,
 };
-use std::{thread, time::Duration};
+use std::fs::File;
 
 fn main() {
     let addr = "127.0.0.1:5000".parse().unwrap();
@@ -24,38 +16,14 @@ fn main() {
 
 fn handle_request(req: Request<Body>) -> Response<Body> {
     println!("Request: {req:#?}");
-    // let (chunked, sender) = ChunkedBody::new();
 
-    // sender.send("<h1>Hello</h1>").ok();
-
-    // thread::spawn(move || {
-    //     thread::sleep(Duration::from_secs(2));
-    //     sender.send("<h2>World</h2>").ok();
-    // });
-
-    let (sender, stream) = SseStream::new();
-
-    thread::spawn(move || {
-        let mut id = 0;
-
-        loop {
-            thread::sleep(Duration::from_secs(1));
-
-            let event = SseEvent::new()
-                .id(id.to_string())
-                .data(format!("New event {id}"))
-                .unwrap();
-
-            sender.send(event).unwrap();
-            id += 1;
-        }
-    });
+    let cwd = std::env::current_dir().unwrap();
+    let file_dir = cwd.join("assets/bocchi.jpg");
+    let file = File::open(file_dir).unwrap();
 
     Response::builder()
         .insert_header("X-Server", "MyServer")
-        .insert_header(headers::CONTENT_TYPE, "text/event-stream")
-        //.insert_header(headers::TRANSFER_ENCODING, "chunked")
-        .insert_header(headers::CONNECTION, "keep-alive")
+        .insert_header(headers::CONTENT_TYPE, "image/jpg")
         .status(StatusCode::OK)
-        .build(stream.into())
+        .build(Body::new(file))
 }
