bdf0038d952363f54495553b1714d77a181b4c77
Freddy L <neo-ciber_94@hotmail.com>
2024-09-29 01:02:03 -0400

    docs

--- packages/http1/src/uri/scheme.rs
+++ packages/http1/src/uri/scheme.rs
@@ -1,18 +1,27 @@
 use std::fmt::Display;
 
+/// Any other scheme: ftp, mysql, etc...
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct AnyScheme(String); // this is just a private type to prevent create a raw scheme without using Scheme::from(...)
+
+/// Represents an URI scheme.
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum Scheme {
+    /// HTTP scheme.
     Http,
+    /// HTTPS scheme.
     Https,
-    Other(String),
+    /// Any other scheme: ftp, mysql, etc...
+    Other(AnyScheme),
 }
 
 impl Scheme {
+    /// Returns the str representation of this scheme.
     pub fn as_str(&self) -> &str {
         match self {
             Scheme::Http => "http",
             Scheme::Https => "https",
-            Scheme::Other(s) => s.as_str(),
+            Scheme::Other(s) => s.0.as_str(),
         }
     }
 }
@@ -22,7 +31,7 @@ impl Display for Scheme {
         match self {
             Scheme::Http => write!(f, "http"),
             Scheme::Https => write!(f, "https"),
-            Scheme::Other(s) => write!(f, "{s}"),
+            Scheme::Other(s) => write!(f, "{}", s.0),
         }
     }
 }
@@ -32,7 +41,7 @@ impl<'a> From<&'a str> for Scheme {
         match value {
             _ if value.eq_ignore_ascii_case("http") => Scheme::Http,
             _ if value.eq_ignore_ascii_case("https") => Scheme::Https,
-            other => Scheme::Other(other.to_owned()),
+            other => Scheme::Other(AnyScheme(other.to_lowercase().to_owned())),
         }
     }
 }
@@ -46,5 +55,33 @@ impl<'a> From<&'a String> for Scheme {
 impl From<String> for Scheme {
     fn from(value: String) -> Self {
         Scheme::from(value.as_str())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::uri::scheme::AnyScheme;
+
+    use super::Scheme;
+
+    #[test]
+    fn should_parse_http_scheme() {
+        assert_eq!(Scheme::from("http"), Scheme::Http);
+        assert_eq!(Scheme::from("HTTP"), Scheme::Http);
+        assert_eq!(Scheme::from("hTTp"), Scheme::Http);
+    }
+
+    #[test]
+    fn should_parse_https_scheme() {
+        assert_eq!(Scheme::from("https"), Scheme::Https);
+        assert_eq!(Scheme::from("HTTPS"), Scheme::Https);
+        assert_eq!(Scheme::from("hTTpS"), Scheme::Https);
+    }
+
+    #[test]
+    fn should_parse_any_scheme() {
+        assert_eq!(Scheme::from("ftp"), Scheme::Other(AnyScheme("ftp".to_owned())));
+        assert_eq!(Scheme::from("POSTGRES"), Scheme::Other(AnyScheme("postgres".to_owned())));
+        assert_eq!(Scheme::from("Redis"), Scheme::Other(AnyScheme("redis".to_owned())));
     }
 }
