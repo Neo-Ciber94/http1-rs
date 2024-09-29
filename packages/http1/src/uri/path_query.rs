8a8f77821f7d3e57fba6706e693169d6e01400d9
Freddy L <neo-ciber_94@hotmail.com>
2024-09-29 01:34:29 -0400

    Add tests

--- packages/http1/src/uri/path_query.rs
+++ packages/http1/src/uri/path_query.rs
@@ -1,7 +1,14 @@
-use std::{collections::HashMap, fmt::Display, str::FromStr};
+use std::{
+    collections::HashMap,
+    fmt::Display,
+    str::{FromStr, Split},
+};
 
 use super::uri::InvalidUri;
 
+/// Represents the path and query from an URI.
+///
+/// `path_query = path ["?" query] ["#" fragment]`
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct PathAndQuery {
     path: String,
@@ -10,7 +17,10 @@ pub struct PathAndQuery {
 }
 
 impl PathAndQuery {
+    /// Constructs a new `PathAndQuery`.
     pub fn new(path: String, query: Option<String>, fragment: Option<String>) -> Self {
+        assert!(path.starts_with("/"), "Path should start with '/'");
+
         PathAndQuery {
             path,
             query,
@@ -18,23 +28,75 @@ impl PathAndQuery {
         }
     }
 
+    /// Constructs a new `PathAndQuery` from the given path.
+    pub fn with_path(path: String) -> Self {
+        Self::new(path, None, None)
+    }
+
+    /// Constructs a new `PathAndQuery` from the given path and query.
+    pub fn with_path_query(path: String, query: String) -> Self {
+        Self::new(path, Some(query), None)
+    }
+
+    /// Returns the path.
     pub fn path(&self) -> &str {
         &self.path
     }
 
+    /// Returns the query if any or `None`.
     pub fn query(&self) -> Option<&str> {
         self.query.as_deref()
     }
 
+    /// Returns the fragment if any or `None`.
     pub fn fragment(&self) -> Option<&str> {
         self.fragment.as_deref()
     }
 
+    /// Returns an iterator over the query values.
     pub fn query_values(&self) -> QueryValues {
         match &self.query {
             Some(s) => QueryValues::Values { iter: s.split("&") },
             None => QueryValues::Empty,
         }
+    }
+
+    /// Create a map over the query values.
+    pub fn query_map(&self) -> QueryMap {
+        let mut map = HashMap::<String, QueryValue>::new();
+
+        for (key, value) in self.query_values() {
+            if map.contains_key(key) {
+                let entry = map.get_mut(key).unwrap();
+                match entry {
+                    QueryValue::One(s) => {
+                        let cur = std::mem::take(s);
+                        let list = vec![cur, value.to_owned()];
+                        *entry = QueryValue::List(list);
+                    }
+                    QueryValue::List(list) => list.push(value.to_owned()),
+                }
+            } else {
+                map.insert(key.to_owned(), QueryValue::One(value.to_string()));
+            }
+        }
+
+        QueryMap(map)
+    }
+
+    /// An iterator over the segments of the path.
+    pub fn segments(&self) -> Segments {
+        let mut p = self.path.as_str();
+
+        if p.starts_with("/") {
+            p = &p[1..];
+        }
+
+        if p.ends_with("/") {
+            p = &p[..(p.len() - 1)];
+        }
+
+        Segments(p.split("/"))
     }
 }
 
@@ -51,6 +113,16 @@ impl Display for PathAndQuery {
         }
 
         Ok(())
+    }
+}
+
+pub struct Segments<'a>(Split<'a, &'a str>);
+
+impl<'a> Iterator for Segments<'a> {
+    type Item = &'a str;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next()
     }
 }
 
@@ -71,30 +143,6 @@ impl<'a> Iterator for QueryValues<'a> {
                 Some((name, value))
             }
         }
-    }
-}
-
-impl QueryValues<'_> {
-    pub fn to_map(self) -> QueryMap {
-        let mut map = HashMap::<String, QueryValue>::new();
-
-        for (key, value) in self {
-            if map.contains_key(key) {
-                let entry = map.get_mut(key).unwrap();
-                match entry {
-                    QueryValue::One(s) => {
-                        let cur = std::mem::take(s);
-                        let list = vec![cur, value.to_owned()];
-                        *entry = QueryValue::List(list);
-                    }
-                    QueryValue::List(list) => list.push(value.to_owned()),
-                }
-            } else {
-                map.insert(key.to_owned(), QueryValue::One(value.to_string()));
-            }
-        }
-
-        QueryMap(map)
     }
 }
 
@@ -196,4 +244,87 @@ fn parse_path_query_string(mut s: String) -> Result<PathAndQuery, InvalidUri> {
     };
 
     Ok(PathAndQuery::new(path, query, fragment))
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use super::PathAndQuery;
+
+    #[test]
+    fn should_parse_path() {
+        let pq = PathAndQuery::from_str("/makoto/ryuji/saki").unwrap();
+
+        assert_eq!(pq.path(), "/makoto/ryuji/saki");
+        assert_eq!(pq.query(), None);
+        assert_eq!(pq.fragment(), None);
+    }
+
+    #[test]
+    fn should_parse_path_and_query() {
+        let pq = PathAndQuery::from_str("/users/?limit=10&sort=email").unwrap();
+
+        assert_eq!(pq.path(), "/users/");
+        assert_eq!(pq.query(), Some("limit=10&sort=email"));
+        assert_eq!(pq.fragment(), None);
+    }
+
+    #[test]
+    fn should_parse_path_and_fragment() {
+        let pq = PathAndQuery::from_str("/erwin#general").unwrap();
+
+        assert_eq!(pq.path(), "/erwin");
+        assert_eq!(pq.query(), None);
+        assert_eq!(pq.fragment(), Some("general"));
+    }
+
+    #[test]
+    fn should_get_query_map() {
+        let pq = PathAndQuery::from_str("/users/?limit=10&sort=email").unwrap();
+
+        let query_map = pq.query_map();
+        assert_eq!(query_map.get("limit"), Some("10"));
+        assert_eq!(query_map.get("sort"), Some("email"));
+    }
+
+    #[test]
+    fn should_get_path_segments() {
+        let p = PathAndQuery::from_str("/one/two/three").unwrap();
+        let mut segments = p.segments();
+
+        assert_eq!(segments.next(), Some("one"));
+        assert_eq!(segments.next(), Some("two"));
+        assert_eq!(segments.next(), Some("three"));
+        assert_eq!(segments.next(), None);
+    }
+
+    #[test]
+    fn should_get_path_segments_ending_in_slash() {
+        let p = PathAndQuery::from_str("/one/two/three/").unwrap();
+        let mut segments = p.segments();
+
+        assert_eq!(segments.next(), Some("one"));
+        assert_eq!(segments.next(), Some("two"));
+        assert_eq!(segments.next(), Some("three"));
+        assert_eq!(segments.next(), None);
+    }
+
+    #[test]
+    fn should_get_path_segments_empty() {
+        let p = PathAndQuery::from_str("/").unwrap();
+        let mut segments = p.segments();
+
+        assert_eq!(segments.next(), Some(""));
+        assert_eq!(segments.next(), None);
+    }
+
+    #[test]
+    fn should_get_single_segment() {
+        let p = PathAndQuery::from_str("/one").unwrap();
+        let mut segments = p.segments();
+
+        assert_eq!(segments.next(), Some("one"));
+        assert_eq!(segments.next(), None);
+    }
 }
