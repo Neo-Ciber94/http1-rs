80badb034d2fd16b8433db5090d69794dedd4574
Freddy L <neo-ciber_94@hotmail.com>
2024-09-29 20:18:35 -0400

    minor changes

--- packages/http1/src/body/http_body.rs
+++ packages/http1/src/body/http_body.rs
@@ -2,15 +2,27 @@ pub trait HttpBody {
     type Err;
     type Data: Into<Vec<u8>>;
 
-    /**
-     * Returns the next chunk of data.
-     */
+    /// Returns the next chunk of data.
     fn read_next(&mut self) -> Result<Option<Self::Data>, Self::Err>;
 
-    /**
-     * Returns the total size of the data to write.
-     */
+    /// Returns the total size of the data to write.
     fn size_hint(&self) -> Option<usize> {
         None
+    }
+
+    /// Read all the chunks and returns a `Vec` containing all the bytes.
+    fn read_all_bytes(&mut self) -> Result<Vec<u8>, Self::Err> {
+        let mut bytes = Vec::new();
+
+        loop {
+            match self.read_next()? {
+                Some(chunk) => {
+                    bytes.extend(chunk.into());
+                }
+                None => break,
+            }
+        }
+
+        Ok(bytes)
     }
 }
