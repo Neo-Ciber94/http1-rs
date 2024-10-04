4a4efe92c95049da6f478b96d0b9e940dcb3a184
Freddy L <neo-ciber_94@hotmail.com>
2024-10-03 20:00:43 -0400

    minor changes

--- packages/http1_web/src/common/serde/json/mod.rs
+++ packages/http1_web/src/common/serde/json/mod.rs
@@ -4,27 +4,34 @@ use ser::{JsonSerializationError, JsonSerializer};
 use super::serialize::Serialize;
 
 pub mod formatter;
+pub mod map;
 pub mod ser;
 pub mod value;
-pub mod map;
 
 pub fn to_bytes<T: Serialize>(value: &T) -> Result<Vec<u8>, JsonSerializationError> {
+    let mut buf = Vec::<u8>::new();
+    let mut serializer = JsonSerializer::new(&mut buf, CompactFormatter);
+    value.serialize(&mut serializer)?;
+    Ok(buf)
+}
+
+pub fn to_pretty_bytes<T: Serialize>(value: &T) -> Result<Vec<u8>, JsonSerializationError> {
     let mut buf = Vec::<u8>::new();
     let mut serializer = JsonSerializer::new(&mut buf, PrettyFormatter::new());
     value.serialize(&mut serializer)?;
     Ok(buf)
 }
 
-pub fn to_pretty_string<T: Serialize>(value: &T) -> Result<String, JsonSerializationError> {
+pub fn to_string<T: Serialize>(value: &T) -> Result<String, JsonSerializationError> {
     let mut buf = Vec::<u8>::new();
-    let mut serializer = JsonSerializer::new(&mut buf, PrettyFormatter::new());
+    let mut serializer = JsonSerializer::new(&mut buf, CompactFormatter);
     value.serialize(&mut serializer)?;
     String::from_utf8(buf).map_err(|err| JsonSerializationError::Other(err.to_string()))
 }
 
-pub fn to_string<T: Serialize>(value: &T) -> Result<String, JsonSerializationError> {
+pub fn to_pretty_string<T: Serialize>(value: &T) -> Result<String, JsonSerializationError> {
     let mut buf = Vec::<u8>::new();
-    let mut serializer = JsonSerializer::new(&mut buf, CompactFormatter);
+    let mut serializer = JsonSerializer::new(&mut buf, PrettyFormatter::new());
     value.serialize(&mut serializer)?;
     String::from_utf8(buf).map_err(|err| JsonSerializationError::Other(err.to_string()))
 }
