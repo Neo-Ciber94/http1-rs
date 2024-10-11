5401e6c01fadd654399bc1e1abe617b1a5226f63
Freddy L <neo-ciber_94@hotmail.com>
2024-10-10 20:48:28 -0400

    format

--- packages/http1/src/common/mod.rs
+++ packages/http1/src/common/mod.rs
@@ -1,5 +1,5 @@
-pub mod map;
 pub mod any_map;
 pub mod date_time;
+pub mod map;
 pub mod thread_pool;
 pub mod thread_spawner;
--- packages/http1_web/src/serde/mod.rs
+++ packages/http1_web/src/serde/mod.rs
@@ -3,5 +3,5 @@ pub mod expected;
 pub mod impossible;
 pub mod json;
 pub mod ser;
-pub mod visitor;
 pub mod string;
+pub mod visitor;
