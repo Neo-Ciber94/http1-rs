b743428d6123edf60163ae71eb78dd471fc616c2
Freddy L <neo-ciber_94@hotmail.com>
2024-10-20 17:53:19 -0400

    Update logging middleware

--- packages/http1_web/src/middleware/logging.rs
+++ packages/http1_web/src/middleware/logging.rs
@@ -11,13 +11,15 @@ impl Middleware for Logging {
     fn on_request(&self, req: Request<Body>, next: &BoxedHandler) -> Response<Body> {
         let method = req.method().clone();
         let uri = req.uri().to_string();
-        let start_time = DateTime::now_utc();
+        let now = DateTime::now_utc();
+
+        log::info!("{method} {uri}");
 
         let response = next.call(req);
         let status_code = response.status();
-        let duration_ms = DateTime::now_utc().millis_since(start_time);
-        let date = start_time.to_iso_8601_string();
-        println!("[{date}] {method} {uri} - {status_code} - {duration_ms}ms");
+        let duration_ms = DateTime::now_utc().millis_since(now);
+
+        log::info!("{status_code} - {duration_ms}ms");
 
         response
     }
