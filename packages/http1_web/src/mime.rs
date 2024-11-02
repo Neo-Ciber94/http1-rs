d2694b9fc131a0c502843c4ed63de7f534314ad4
Freddy L <neo-ciber_94@hotmail.com>
2024-11-02 12:48:28 -0400

    Update mime

--- packages/http1_web/src/mime.rs
+++ packages/http1_web/src/mime.rs
@@ -2,6 +2,9 @@ use std::{borrow::Cow, fmt::Display, str::FromStr};
 
 use http1::headers::HeaderValue;
 
+/// Represents document format: https://developer.mozilla.org/en-US/docs/Web/HTTP/MIME_types
+///
+/// A mime is compose by: `type/subtype;<parameter=value>`
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Mime {
     type_: Cow<'static, str>,
@@ -48,6 +51,7 @@ impl Mime {
         })
     }
 
+    /// Constructs a new mime type.
     pub fn new(
         type_: impl Into<Cow<'static, str>>,
         subtype: impl Into<Cow<'static, str>>,
@@ -67,10 +71,12 @@ impl Mime {
         }
     }
 
+    /// Constructs a mime type from an extension.
     pub fn from_extension(file_extension: &str) -> Result<Mime, &str> {
         Mime::get_mime_from_extension(file_extension).ok_or(file_extension)
     }
 
+    /// Constructs a mime type from a file name.
     pub fn guess_mime(filename: &str) -> Result<Mime, &str> {
         if let Some(extension) = filename.split('.').last() {
             Mime::from_extension(extension)
@@ -79,21 +85,25 @@ impl Mime {
         }
     }
 
+    /// Returns the `type` part of the mime.
     #[inline]
     pub fn ty(&self) -> &str {
         self.type_.as_ref()
     }
 
+    /// Returns the `subtype` part of the mime.
     #[inline]
     pub fn subtype(&self) -> &str {
         self.subtype.as_ref()
     }
 
+    /// Returns the parameter of this mime.
     #[inline]
     pub fn parameter(&self) -> Option<&str> {
         self.parameter.as_deref()
     }
 
+    /// Check if this mime matches the other.
     pub fn matches(&self, other: &Mime) -> bool {
         match (self.ty(), other.ty()) {
             ("*", "*") | ("*", _) | (_, "*") => {}
@@ -229,9 +239,30 @@ macro_rules! declare_mime_types {
                     return None;
                 }
 
+                // I'm counting on the compiler to remove the Option::is_none(None)
                 match extension {
                     $(
-                        $EXT => Some(Mime::$NAME),
+                        $EXT if Option::<&str>::is_none(&$($PARAM)*) => Some(Mime::$NAME),
+                    )*
+                    _ => None
+                }
+            }
+
+            /// Returns the extension for this mime.
+            pub fn extension(&self) -> Option<&str> {
+                match (self.ty(), self.subtype(), self.parameter()) {
+                    $(
+                        ($TYPE, $SUBTYPE, $($PARAM)*) => Some($EXT),
+                    )*
+                    _ => None
+                }
+            }
+
+            /// Returns the mime extension with its dot separator.
+            pub fn extension_with_sep(&self)-> Option<&str> {
+                match (self.ty(), self.subtype(), self.parameter()) {
+                    $(
+                        ($TYPE, $SUBTYPE, $($PARAM)*) => Some(concat!(".", $EXT)),
                     )*
                     _ => None
                 }
@@ -244,9 +275,9 @@ declare_mime_types! {
     // Application types
     APPLICATION_OCTET_STREAM => ["bin", "application", "octet-stream", None],
     APPLICATION_JSON => ["json", "application", "json", None],
-    APPLICATION_JSON_UTF8 => ["", "application", "json", Some("charset=UTF-8")],
+    APPLICATION_JSON_UTF8 => ["json", "application", "json", Some("charset=UTF-8")],
     APPLICATION_JAVASCRIPT => ["js", "application", "javascript", None],
-    APPLICATION_XML => ["", "application", "xml", None],
+    APPLICATION_XML => ["xml", "application", "xml", None],
     APPLICATION_PDF => ["pdf", "application", "pdf", None],
     APPLICATION_ZIP => ["zip", "application", "zip", None],
     APPLICATION_RAR => ["rar", "application", "vnd.rar", None],
@@ -346,6 +377,11 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_constant() {
+
+    }
+
+    #[test]
     fn should_create_valid_mime_type() {
         let mime = Mime::new("text", "plain", None);
         assert_eq!(mime.ty(), "text");
@@ -399,6 +435,12 @@ mod tests {
     }
 
     #[test]
+    fn should_guess_extension_from_file_with_multiple() {
+        let mime = Mime::guess_mime("foo.tar.gz").unwrap();
+        assert_eq!(mime.to_string(), "application/gzip");
+    }
+
+    #[test]
     fn should_parse_valid_mime_from_string() {
         let mime: Mime = "audio/mpeg".parse().unwrap();
         assert_eq!(mime.ty(), "audio");
@@ -414,13 +456,13 @@ mod tests {
     #[test]
     fn should_display_mime_as_string_without_parameter() {
         let mime = Mime::new("video", "mp4", None);
-        assert_eq!(format!("{}", mime), "video/mp4");
+        assert_eq!(mime.to_string(), "video/mp4");
     }
 
     #[test]
     fn should_display_mime_as_string_with_parameter() {
         let mime = Mime::new("application", "xml", Some("charset=UTF-8"));
-        assert_eq!(format!("{}", mime), "application/xml;charset=UTF-8");
+        assert_eq!(mime.to_string(), "application/xml;charset=UTF-8");
     }
 
     #[test]
@@ -487,5 +529,161 @@ mod tests {
         let mime21 = Mime::new("*", "*", None);
         let mime22 = Mime::new("application", "json", None);
         assert!(mime21.matches(&mime22));
+    }
+
+    #[test]
+    fn should_return_correct_extension() {
+        assert_eq!(Mime::APPLICATION_OCTET_STREAM.extension(), Some("bin"));
+        assert_eq!(Mime::APPLICATION_JSON.extension(), Some("json"));
+        assert_eq!(Mime::APPLICATION_JSON_UTF8.extension(), Some("json"));
+        assert_eq!(Mime::APPLICATION_JAVASCRIPT.extension(), Some("js"));
+        assert_eq!(Mime::APPLICATION_XML.extension(), Some("xml"));
+        assert_eq!(Mime::APPLICATION_PDF.extension(), Some("pdf"));
+        assert_eq!(Mime::APPLICATION_ZIP.extension(), Some("zip"));
+        assert_eq!(Mime::APPLICATION_RAR.extension(), Some("rar"));
+        assert_eq!(Mime::APPLICATION_7Z.extension(), Some("7z"));
+        assert_eq!(Mime::APPLICATION_GZIP.extension(), Some("gz"));
+        assert_eq!(Mime::APPLICATION_RTF.extension(), Some("rtf"));
+        assert_eq!(Mime::APPLICATION_SQL.extension(), Some("sql"));
+        assert_eq!(Mime::APPLICATION_WASM.extension(), Some("wasm"));
+        assert_eq!(Mime::APPLICATION_XHTML.extension(), Some("xhtml"));
+        assert_eq!(Mime::APPLICATION_TAR.extension(), Some("tar"));
+        assert_eq!(Mime::APPLICATION_MSWORD.extension(), Some("doc"));
+        assert_eq!(Mime::APPLICATION_MSWORD_OPENXML.extension(), Some("docx"));
+        assert_eq!(Mime::APPLICATION_POWERPOINT.extension(), Some("ppt"));
+        assert_eq!(
+            Mime::APPLICATION_POWERPOINT_OPENXML.extension(),
+            Some("pptx")
+        );
+        assert_eq!(Mime::APPLICATION_EXCEL.extension(), Some("xls"));
+        assert_eq!(Mime::APPLICATION_EXCEL_OPENXML.extension(), Some("xlsx"));
+        assert_eq!(Mime::APPLICATION_EPUB.extension(), Some("epub"));
+        assert_eq!(Mime::APPLICATION_OGG.extension(), Some("ogx"));
+        assert_eq!(Mime::TEXT_PLAIN.extension(), Some("txt"));
+        assert_eq!(Mime::TEXT_HTML.extension(), Some("html"));
+        assert_eq!(Mime::TEXT_CSS.extension(), Some("css"));
+        assert_eq!(Mime::TEXT_CSV.extension(), Some("csv"));
+        assert_eq!(Mime::TEXT_XML.extension(), Some("xml"));
+        assert_eq!(Mime::TEXT_MARKDOWN.extension(), Some("md"));
+        assert_eq!(Mime::TEXT_YAML.extension(), Some("yaml"));
+        assert_eq!(Mime::TEXT_VCARD.extension(), Some("vcf"));
+        assert_eq!(Mime::IMAGE_JPEG.extension(), Some("jpg"));
+        assert_eq!(Mime::IMAGE_PNG.extension(), Some("png"));
+        assert_eq!(Mime::IMAGE_GIF.extension(), Some("gif"));
+        assert_eq!(Mime::IMAGE_SVG.extension(), Some("svg"));
+        assert_eq!(Mime::IMAGE_TIFF.extension(), Some("tiff"));
+        assert_eq!(Mime::IMAGE_WEBP.extension(), Some("webp"));
+        assert_eq!(Mime::IMAGE_BMP.extension(), Some("bmp"));
+        assert_eq!(Mime::IMAGE_ICO.extension(), Some("ico"));
+        assert_eq!(Mime::IMAGE_HEIF.extension(), Some("heif"));
+        assert_eq!(Mime::IMAGE_HEIC.extension(), Some("heic"));
+        assert_eq!(Mime::AUDIO_MP3.extension(), Some("mp3"));
+        assert_eq!(Mime::AUDIO_WAV.extension(), Some("wav"));
+        assert_eq!(Mime::AUDIO_OGG.extension(), Some("ogg"));
+        assert_eq!(Mime::AUDIO_FLAC.extension(), Some("flac"));
+        assert_eq!(Mime::AUDIO_MIDI.extension(), Some("midi"));
+        assert_eq!(Mime::AUDIO_WEBM.extension(), Some("weba"));
+        assert_eq!(Mime::AUDIO_AAC.extension(), Some("aac"));
+        assert_eq!(Mime::AUDIO_M4A.extension(), Some("m4a"));
+        assert_eq!(Mime::VIDEO_MP4.extension(), Some("mp4"));
+        assert_eq!(Mime::VIDEO_WEBM.extension(), Some("webm"));
+        assert_eq!(Mime::VIDEO_OGG.extension(), Some("ogv"));
+        assert_eq!(Mime::VIDEO_MPEG.extension(), Some("mpeg"));
+        assert_eq!(Mime::VIDEO_3GP.extension(), Some("3gp"));
+        assert_eq!(Mime::VIDEO_FLV.extension(), Some("flv"));
+        assert_eq!(Mime::VIDEO_AVI.extension(), Some("avi"));
+        assert_eq!(Mime::VIDEO_MOV.extension(), Some("mov"));
+        assert_eq!(Mime::VIDEO_MKV.extension(), Some("mkv"));
+        assert_eq!(Mime::FONT_TTF.extension(), Some("ttf"));
+        assert_eq!(Mime::FONT_OTF.extension(), Some("otf"));
+        assert_eq!(Mime::FONT_WOFF.extension(), Some("woff"));
+        assert_eq!(Mime::FONT_WOFF2.extension(), Some("woff2"));
+    }
+
+    #[test]
+    fn should_return_correct_extension_with_separator() {
+        assert_eq!(
+            Mime::APPLICATION_OCTET_STREAM.extension_with_sep(),
+            Some(".bin")
+        );
+        assert_eq!(Mime::APPLICATION_JSON.extension_with_sep(), Some(".json"));
+        assert_eq!(
+            Mime::APPLICATION_JSON_UTF8.extension_with_sep(),
+            Some(".json")
+        );
+        assert_eq!(
+            Mime::APPLICATION_JAVASCRIPT.extension_with_sep(),
+            Some(".js")
+        );
+        assert_eq!(Mime::APPLICATION_XML.extension_with_sep(), Some(".xml"));
+        assert_eq!(Mime::APPLICATION_PDF.extension_with_sep(), Some(".pdf"));
+        assert_eq!(Mime::APPLICATION_ZIP.extension_with_sep(), Some(".zip"));
+        assert_eq!(Mime::APPLICATION_RAR.extension_with_sep(), Some(".rar"));
+        assert_eq!(Mime::APPLICATION_7Z.extension_with_sep(), Some(".7z"));
+        assert_eq!(Mime::APPLICATION_GZIP.extension_with_sep(), Some(".gz"));
+        assert_eq!(Mime::APPLICATION_RTF.extension_with_sep(), Some(".rtf"));
+        assert_eq!(Mime::APPLICATION_SQL.extension_with_sep(), Some(".sql"));
+        assert_eq!(Mime::APPLICATION_WASM.extension_with_sep(), Some(".wasm"));
+        assert_eq!(Mime::APPLICATION_XHTML.extension_with_sep(), Some(".xhtml"));
+        assert_eq!(Mime::APPLICATION_TAR.extension_with_sep(), Some(".tar"));
+        assert_eq!(Mime::APPLICATION_MSWORD.extension_with_sep(), Some(".doc"));
+        assert_eq!(
+            Mime::APPLICATION_MSWORD_OPENXML.extension_with_sep(),
+            Some(".docx")
+        );
+        assert_eq!(
+            Mime::APPLICATION_POWERPOINT.extension_with_sep(),
+            Some(".ppt")
+        );
+        assert_eq!(
+            Mime::APPLICATION_POWERPOINT_OPENXML.extension_with_sep(),
+            Some(".pptx")
+        );
+        assert_eq!(Mime::APPLICATION_EXCEL.extension_with_sep(), Some(".xls"));
+        assert_eq!(
+            Mime::APPLICATION_EXCEL_OPENXML.extension_with_sep(),
+            Some(".xlsx")
+        );
+        assert_eq!(Mime::APPLICATION_EPUB.extension_with_sep(), Some(".epub"));
+        assert_eq!(Mime::APPLICATION_OGG.extension_with_sep(), Some(".ogx"));
+        assert_eq!(Mime::TEXT_PLAIN.extension_with_sep(), Some(".txt"));
+        assert_eq!(Mime::TEXT_HTML.extension_with_sep(), Some(".html"));
+        assert_eq!(Mime::TEXT_CSS.extension_with_sep(), Some(".css"));
+        assert_eq!(Mime::TEXT_CSV.extension_with_sep(), Some(".csv"));
+        assert_eq!(Mime::TEXT_XML.extension_with_sep(), Some(".xml"));
+        assert_eq!(Mime::TEXT_MARKDOWN.extension_with_sep(), Some(".md"));
+        assert_eq!(Mime::TEXT_YAML.extension_with_sep(), Some(".yaml"));
+        assert_eq!(Mime::TEXT_VCARD.extension_with_sep(), Some(".vcf"));
+        assert_eq!(Mime::IMAGE_JPEG.extension_with_sep(), Some(".jpg"));
+        assert_eq!(Mime::IMAGE_PNG.extension_with_sep(), Some(".png"));
+        assert_eq!(Mime::IMAGE_GIF.extension_with_sep(), Some(".gif"));
+        assert_eq!(Mime::IMAGE_SVG.extension_with_sep(), Some(".svg"));
+        assert_eq!(Mime::IMAGE_TIFF.extension_with_sep(), Some(".tiff"));
+        assert_eq!(Mime::IMAGE_WEBP.extension_with_sep(), Some(".webp"));
+        assert_eq!(Mime::IMAGE_BMP.extension_with_sep(), Some(".bmp"));
+        assert_eq!(Mime::IMAGE_ICO.extension_with_sep(), Some(".ico"));
+        assert_eq!(Mime::IMAGE_HEIF.extension_with_sep(), Some(".heif"));
+        assert_eq!(Mime::IMAGE_HEIC.extension_with_sep(), Some(".heic"));
+        assert_eq!(Mime::AUDIO_MP3.extension_with_sep(), Some(".mp3"));
+        assert_eq!(Mime::AUDIO_WAV.extension_with_sep(), Some(".wav"));
+        assert_eq!(Mime::AUDIO_OGG.extension_with_sep(), Some(".ogg"));
+        assert_eq!(Mime::AUDIO_FLAC.extension_with_sep(), Some(".flac"));
+        assert_eq!(Mime::AUDIO_MIDI.extension_with_sep(), Some(".midi"));
+        assert_eq!(Mime::AUDIO_WEBM.extension_with_sep(), Some(".weba"));
+        assert_eq!(Mime::AUDIO_AAC.extension_with_sep(), Some(".aac"));
+        assert_eq!(Mime::AUDIO_M4A.extension_with_sep(), Some(".m4a"));
+        assert_eq!(Mime::VIDEO_MP4.extension_with_sep(), Some(".mp4"));
+        assert_eq!(Mime::VIDEO_WEBM.extension_with_sep(), Some(".webm"));
+        assert_eq!(Mime::VIDEO_OGG.extension_with_sep(), Some(".ogv"));
+        assert_eq!(Mime::VIDEO_MPEG.extension_with_sep(), Some(".mpeg"));
+        assert_eq!(Mime::VIDEO_3GP.extension_with_sep(), Some(".3gp"));
+        assert_eq!(Mime::VIDEO_FLV.extension_with_sep(), Some(".flv"));
+        assert_eq!(Mime::VIDEO_AVI.extension_with_sep(), Some(".avi"));
+        assert_eq!(Mime::VIDEO_MOV.extension_with_sep(), Some(".mov"));
+        assert_eq!(Mime::VIDEO_MKV.extension_with_sep(), Some(".mkv"));
+        assert_eq!(Mime::FONT_TTF.extension_with_sep(), Some(".ttf"));
+        assert_eq!(Mime::FONT_OTF.extension_with_sep(), Some(".otf"));
+        assert_eq!(Mime::FONT_WOFF.extension_with_sep(), Some(".woff"));
+        assert_eq!(Mime::FONT_WOFF2.extension_with_sep(), Some(".woff2"));
     }
 }
