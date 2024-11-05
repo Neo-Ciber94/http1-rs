e9b9a0de1ccd66d5b92571ead6cb0631dd44f4e7
Freddy L <neo-ciber_94@hotmail.com>
2024-11-04 22:27:49 -0400

    minor changes

--- packages/http1_web/src/middleware/logging.rs
+++ packages/http1_web/src/middleware/logging.rs
@@ -1,6 +1,7 @@
+use std::time::Instant;
+
 use crate::handler::BoxedHandler;
 use crate::middleware::Middleware;
-use datetime::DateTime;
 use http1::body::Body;
 use http1::request::Request;
 use http1::response::Response;
@@ -11,13 +12,13 @@ impl Middleware for Logging {
     fn on_request(&self, req: Request<Body>, next: &BoxedHandler) -> Response<Body> {
         let method = req.method().clone();
         let uri = req.uri().to_string();
-        let now = DateTime::now_utc();
+        let now = Instant::now();
 
         log::info!("Request: {method} {uri}");
 
         let response = next.call(req);
         let status_code = response.status();
-        let duration_ms = DateTime::now_utc().millis_since(now);
+        let duration_ms = now.elapsed().as_millis();
 
         let level = if status_code.is_client_error() {
             log::LogLevel::Warn
