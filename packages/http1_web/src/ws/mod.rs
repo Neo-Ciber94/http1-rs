c978333d946891c731d27123645fe04b500318bd
Freddy L <neo-ciber_94@hotmail.com>
2024-11-12 23:02:58 -0400

    Update websocket impl

--- packages/http1_web/src/ws/mod.rs
+++ packages/http1_web/src/ws/mod.rs
@@ -1,12 +1,68 @@
 use std::fmt::Display;
+use std::net::TcpStream;
+use std::str::FromStr;
 
-use http1::{headers, method::Method, status::StatusCode};
+use http1::{
+    body::Body, common::base64::encode, error::BoxError, headers, method::Method, request::Request,
+    response::Response, status::StatusCode,
+};
 
 use crate::{from_request::FromRequestRef, IntoResponse};
 
 const WEB_SOCKET_VERSION: &str = "13";
+const WEB_SOCKET_UUID_STR: &str = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
 
-pub struct WebSocketUpgrade;
+// This should be a stream send by the server to keep the connection alive for the messaging.
+type Upgrade = TcpStream;
+
+#[derive(Debug)]
+pub struct WebSocketUpgrade {
+    key: String,
+    version: u16,
+}
+
+impl WebSocketUpgrade {
+    fn upgrade<F>(self, mut req: Request<Body>, mut f: F) -> Response<Body>
+    where
+        F: FnMut(WebSocket) + Send + 'static,
+    {
+        let WebSocketUpgrade { key, .. } = self;
+        let accept_sha1 = sha1(format!("{key}{WEB_SOCKET_UUID_STR}"));
+        let accept_key = encode(accept_sha1).expect("failed to base64 encode accept sha1 hash");
+
+        // Handle the websocket messaging in other thread
+        {
+            let upgrade = req
+                .extensions_mut()
+                .remove::<Upgrade>()
+                .expect("Failed to get websocket upgrade connection from extensions");
+
+            std::thread::spawn(move || f(WebSocket(upgrade)));
+        }
+
+        Response::builder()
+            .status(StatusCode::SWITCHING_PROTOCOLS)
+            .insert_header(headers::UPGRADE, "websocket")
+            .insert_header(headers::CONNECTION, "upgrade")
+            .insert_header(headers::SEC_WEBSOCKET_ACCEPT, accept_key)
+            .body(Body::empty())
+    }
+}
+
+fn sha1(s: String) -> String {
+    s
+}
+
+pub struct WebSocket(Upgrade);
+impl WebSocket {
+    pub fn read(&mut self) {
+        todo!()
+    }
+
+    pub fn send(&mut self) {
+        todo!()
+    }
+}
 
 #[derive(Debug)]
 pub enum WebSocketUpgradeError {
@@ -16,8 +72,11 @@ pub enum WebSocketUpgradeError {
     MissingConnectionUpgrade,
     MissingKey,
     MissingVersion,
-    MissingExtensions,
+    NoProtocolsSupported(String),
+    NoExtensionsSupported(String),
     InvalidVersion(String),
+    InvalidKey(String),
+    Other(BoxError),
 }
 
 impl Display for WebSocketUpgradeError {
@@ -39,12 +98,22 @@ impl Display for WebSocketUpgradeError {
             WebSocketUpgradeError::MissingVersion => {
                 write!(f, "Missing `Sec-WebSocket-Version` header")
             }
-            WebSocketUpgradeError::MissingExtensions => {
-                write!(f, "Missing `Sec-WebSocket-Extensions` header")
-            }
             WebSocketUpgradeError::InvalidVersion(version) => write!(
                 f,
                 "Invalid version expected `{WEB_SOCKET_VERSION}` but was `{version}`"
+            ),
+            WebSocketUpgradeError::InvalidKey(key) => write!(
+                f,
+                "Invalid key: `{key}`"
+            ),
+            WebSocketUpgradeError::Other(error) => write!(f, "{error}"),
+            WebSocketUpgradeError::NoProtocolsSupported(protocols) => write!(
+                f,
+                "`Sec-WebSocket-Protocol` was found but not protocol are supported: {protocols}"
+            ),
+            WebSocketUpgradeError::NoExtensionsSupported(extensions) => write!(
+                f,
+                "`Sec-WebSocket-Extensions` was found but not extensions are supported: {extensions}"
             ),
         }
     }
@@ -70,17 +139,14 @@ impl FromRequestRef for WebSocketUpgrade {
         }
 
         let headers = req.headers();
-        let upgrade_header = headers.get(headers::UPGRADE);
 
-        if let Some(upgrade) = upgrade_header {
+        if let Some(upgrade) = headers.get(headers::UPGRADE) {
             if upgrade.as_str().eq_ignore_ascii_case("websocket") {
                 return Err(WebSocketUpgradeError::MissingUpgrade);
             }
         }
 
-        let connection_header = headers.get(headers::CONNECTION);
-
-        if let Some(conn) = connection_header {
+        if let Some(conn) = headers.get(headers::CONNECTION) {
             if conn.as_str().eq_ignore_ascii_case("Upgrade") {
                 return Err(WebSocketUpgradeError::MissingConnectionUpgrade);
             }
@@ -94,18 +160,38 @@ impl FromRequestRef for WebSocketUpgrade {
             .get(headers::SEC_WEBSOCKET_VERSION)
             .ok_or(WebSocketUpgradeError::MissingVersion)?;
 
-        let web_socket_exts = headers
-            .get(headers::SEC_WEBSOCKET_EXTENSIONS)
-            .ok_or(WebSocketUpgradeError::MissingExtensions)?;
-
         if web_socket_version != WEB_SOCKET_VERSION {
             return Err(WebSocketUpgradeError::InvalidVersion(
                 web_socket_version.as_str().to_string(),
             ));
         }
 
-        let web_socket_protocols = headers.get(headers::SEC_WEBSOCKET_PROTOCOL);
+        let key = http1::common::base64::decode(web_socket_key).map_err(|err| {
+            WebSocketUpgradeError::Other(format!("Failed to parse websocket key: {err}").into())
+        })?;
 
-        todo!()
+        if key.as_bytes().len() != 13 {
+            return Err(WebSocketUpgradeError::InvalidKey(key));
+        }
+
+        let version = u16::from_str(web_socket_version.as_str()).map_err(|err| {
+            WebSocketUpgradeError::Other(
+                format!("Failed to parse websocket version `{web_socket_version}`: {err}").into(),
+            )
+        })?;
+
+        if let Some(protocols) = headers.get(headers::SEC_WEBSOCKET_PROTOCOL) {
+            return Err(WebSocketUpgradeError::NoProtocolsSupported(
+                protocols.to_string(),
+            ));
+        }
+
+        if let Some(exts) = headers.get(headers::SEC_WEBSOCKET_EXTENSIONS) {
+            return Err(WebSocketUpgradeError::NoExtensionsSupported(
+                exts.to_string(),
+            ));
+        }
+
+        Ok(WebSocketUpgrade { key, version })
     }
 }
