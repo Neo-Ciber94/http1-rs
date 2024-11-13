3b1e77c6d8532185ca1f57601a31f96c6ed66dc4
Freddy L <neo-ciber_94@hotmail.com>
2024-11-12 21:40:47 -0400

    Update client

--- packages/http1/src/body/chunked_body.rs
+++ packages/http1/src/body/chunked_body.rs
@@ -91,9 +91,6 @@ where
     fn read_line(&mut self, expected_len: usize) -> std::io::Result<Option<Vec<u8>>> {
         self.buf.reserve(expected_len + 2);
 
-        // clear buffer
-        self.buf.clear();
-
         let bytes_read = self.reader.read_until(b'\n', &mut self.buf)?;
 
         match bytes_read {
@@ -117,6 +114,19 @@ where
         }
     }
 
+    fn read_empty_line(&mut self) -> std::io::Result<()> {
+        if let Some(line) = self.read_line(0)? {
+            if !line.is_empty() {
+                return Err(std::io::Error::other(format!(
+                    "expected chunk ending `\r\n` but was `\r\n{:?}`",
+                    String::from_utf8_lossy(&line)
+                )));
+            }
+        }
+
+        Ok(())
+    }
+
     fn read_chunk_size(&mut self) -> std::io::Result<usize> {
         match self.read_line(1)? {
             Some(bytes) => {
@@ -129,12 +139,21 @@ where
     }
 
     fn read_chunk(&mut self, size: usize) -> std::io::Result<Vec<u8>> {
-        match self.read_line(size)? {
-            Some(chunk) => Ok(chunk),
-            None => Err(std::io::Error::other(format!(
-                "Expected `{size}` bytes chunk but was empty"
-            ))),
+        let mut buf = vec![0; size + 2].into_boxed_slice();
+        self.reader.read_exact(&mut buf)?;
+
+        if !buf.ends_with(b"\r\n") {
+            return Err(std::io::Error::other(format!(
+                "expected chunk ending `\r\n` after: `{:?}`",
+                String::from_utf8_lossy(&buf)
+            )));
         }
+
+        let mut rest = buf.to_vec();
+        rest.pop();
+        rest.pop();
+
+        Ok(rest)
     }
 }
 
@@ -154,25 +173,64 @@ impl<R: Read> HttpBody for ReadChunkedBody<R> {
             Ok(Some(chunk))
         } else {
             self.eof = true;
-
-            // Ensure this is the chunk end
-            match self.read_line(0)? {
-                Some(chunk) => {
-                    if chunk != b"\r\n" {
-                        return Err(std::io::Error::other(format!(
-                            "expected chunk ending `\r\n` but was `{:?}`",
-                            String::from_utf8_lossy(&chunk)
-                        )));
-                    }
-                }
-                None => {
-                    return Err(std::io::Error::other(
-                        "expected chunk ending `\r\n` but was empty",
-                    ))
-                }
-            }
-
+            self.read_empty_line()?;
             Ok(None)
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io::{BufReader, Read};
+
+    use crate::body::{body_reader::BodyReader, Body};
+
+    use super::{ChunkedBody, ReadChunkedBody};
+
+    #[track_caller]
+    fn read_exact(mut reader: impl Read, size: usize) -> String {
+        let mut buf = vec![0; size].into_boxed_slice();
+        reader.read_exact(&mut buf).unwrap();
+        String::from_utf8(buf.to_vec()).unwrap()
+    }
+
+    #[test]
+    fn should_send_chunked_body() {
+        let (body, sender) = ChunkedBody::new();
+
+        std::thread::spawn(move || {
+            for i in 1..=3 {
+                sender.send(format!("Chunk {i}")).unwrap();
+            }
+        });
+
+        let body = Body::new(body);
+
+        let mut reader = BufReader::new(BodyReader::new(body));
+
+        assert_eq!(read_exact(&mut reader, 3), "7\r\n");
+        assert_eq!(read_exact(&mut reader, 9), "Chunk 1\r\n");
+
+        assert_eq!(read_exact(&mut reader, 3), "7\r\n");
+        assert_eq!(read_exact(&mut reader, 9), "Chunk 2\r\n");
+
+        assert_eq!(read_exact(&mut reader, 3), "7\r\n");
+        assert_eq!(read_exact(&mut reader, 9), "Chunk 3\r\n");
+
+        assert_eq!(read_exact(&mut reader, 5), "0\r\n\r\n");
+    }
+
+    #[test]
+    fn should_read_chunked_body() {
+        let data = "7\r\nChunk 1\r\n7\r\nChunk 2\r\n7\r\nChunk 3\r\n0\r\n\r\n";
+        let buf_reader = BufReader::new(data.as_bytes());
+
+        let read_chunked_body = ReadChunkedBody::new(buf_reader);
+        let body = Body::new(read_chunked_body);
+        let mut reader = BodyReader::new(body);
+
+        assert_eq!(read_exact(&mut reader, 7), "Chunk 1");
+        assert_eq!(read_exact(&mut reader, 7), "Chunk 2");
+        assert_eq!(read_exact(&mut reader, 7), "Chunk 3");
     }
 }
