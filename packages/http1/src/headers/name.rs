6e563cba26c871ab59524dbcd87fa41a581f5cf2
Freddy L <neo-ciber_94@hotmail.com>
2024-11-12 22:01:58 -0400

    Make header name case insensitive

--- packages/http1/src/headers/name.rs
+++ packages/http1/src/headers/name.rs
@@ -43,6 +43,18 @@ impl PartialEq for HeaderName {
     }
 }
 
+impl<'a> PartialEq<&'a str> for HeaderName {
+    fn eq(&self, other: &&'a str) -> bool {
+        self.0.eq_ignore_ascii_case(other)
+    }
+}
+
+impl<'a> PartialEq<&'a String> for HeaderName {
+    fn eq(&self, other: &&'a String) -> bool {
+        self.0.eq_ignore_ascii_case(other)
+    }
+}
+
 impl Hash for HeaderName {
     fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
         self.0
@@ -71,5 +83,39 @@ impl<'a> From<&'a str> for HeaderName {
             Some(header_name) => header_name,
             None => HeaderName(Cow::Owned(value.into())),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_create_from_static_str() {
+        let name = HeaderName::from_static("Content-Type");
+        assert_eq!(name.as_str(), "Content-Type");
+    }
+
+    #[test]
+    fn should_create_from_string() {
+        let name = HeaderName::from_string("Custom-Header".to_string());
+        assert_eq!(name.as_str(), "Custom-Header");
+    }
+
+    #[test]
+    fn should_compare_case_insensitively_with_another_header_name() {
+        let name1 = HeaderName::from_static("Accept-Encoding");
+        let name2 = HeaderName::from_static("accept-encoding");
+        assert_eq!(name1, name2);
+
+        let name3 = HeaderName::from_static("Content-Type");
+        assert_ne!(name1, name3);
+    }
+
+    #[test]
+    fn should_compare_case_insensitively_with_str_and_string() {
+        let name = HeaderName::from_static("User-Agent");
+        assert_eq!(name, "user-agent");
+        assert_eq!(name, &"USER-AGENT".to_string());
     }
 }
