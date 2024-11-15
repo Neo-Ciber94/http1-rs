20bc70247e9261ef37efbb1324b008deaa6eca30
Freddy L <neo-ciber_94@hotmail.com>
2024-11-14 22:27:29 -0400

    Update logic for reading frame

--- packages/http1_web/src/ws/ws.rs
+++ packages/http1_web/src/ws/ws.rs
@@ -56,6 +56,16 @@ impl WebSocket {
             Ok((mask, len))
         } else if len == 127 {
             let bytes = self.read_exact(8)?;
+            len = (b1 as u128)
+                & (bytes[0] >> 8) as u128
+                & (bytes[1] >> 16) as u128
+                & (bytes[2] >> 24) as u128
+                & (bytes[3] >> 32) as u128
+                & (bytes[4] >> 40) as u128
+                & (bytes[5] >> 48) as u128
+                & (bytes[6] >> 54) as u128
+                & (bytes[7] >> 64) as u128;
+
             Ok((mask, len))
         } else {
             Err(format!("Invalid payload len: `{len}`").into())
@@ -63,11 +73,38 @@ impl WebSocket {
     }
 
     fn read_masking_key(&mut self) -> Result<u32, BoxError> {
-        todo!()
+        let bytes = self.read_exact(4)?;
+        Ok(bytes[0] as u32
+            & ((bytes[1] >> 8) as u32)
+            & ((bytes[2] >> 16) as u32)
+            & ((bytes[3] >> 24) as u32))
     }
 
-    fn read_payload(&mut self, len: u128, masking_key: u32) -> Result<Vec<u8>, BoxError> {
-        todo!()
+    fn read_payload(&mut self, mut len: u128, masking_key: u32) -> Result<Vec<u8>, BoxError> {
+        let mut data = Vec::new();
+        let masking_key_bytes = masking_key.to_be_bytes();
+
+        while len > 0 {
+            let bytes_read = self.upgrade.read(&mut self.buf)?;
+            match bytes_read {
+                0 => break,
+                n => {
+                    len -= n as u128;
+                    let mut bytes = &mut self.buf[..n];
+
+                    if masking_key != 0 {
+                        bytes.iter_mut().enumerate().for_each(|(idx, b)| {
+                            let mask_byte = masking_key_bytes[idx % 4];
+                            *b = *b ^ mask_byte;
+                        });
+                    }
+
+                    data.extend_from_slice(bytes);
+                }
+            }
+        }
+
+        Ok(data)
     }
 
     fn read_frame(&mut self) -> Result<Frame, BoxError> {
@@ -87,44 +124,64 @@ impl WebSocket {
             return Err(String::from("client to server payload should always be encoded").into());
         }
 
-        todo!()
-        // let payload_len = self.read_frame_len()?;
-        // let masking_key = self.read_masking_key()?;
-        // let payload = self.read_payload(payload_len, masking_key)?;
+        let masking_key = if mask { self.read_masking_key()? } else { 0 };
+        let payload = self.read_payload(payload_len, masking_key)?;
 
-        // Ok(Frame {
-        //     fin,
-        //     rsv1,
-        //     rsv2,
-        //     rsv3,
-        //     op_code,
-        //     mask,
-        //     payload_len,
-        //     masking_key,
-        //     payload,
-        // })
+        Ok(Frame {
+            fin,
+            rsv1,
+            rsv2,
+            rsv3,
+            op_code,
+            mask,
+            payload_len,
+            masking_key,
+            payload,
+        })
     }
 
-    // pub fn read(&mut self) -> Result<Message, BoxError> {
-    //     let mut bytes = Vec::new();
+    pub fn read(&mut self) -> Result<Message, BoxError> {
+        let mut msg_data = Vec::new();
+        let mut msg_op_code: Option<OpCode> = None;
 
-    //     loop {
-    //         let read_bytes = self.upgrade.read(&mut self.buf)?;
-    //         match read_bytes {
-    //             0 => {}
-    //             n => {}
-    //         }
-    //     }
+        loop {
+            let Frame {
+                fin,
+                op_code,
+                payload,
+                ..
+            } = self.read_frame()?;
 
-    //     todo!()
-    // }
+            msg_data.extend(payload);
+            msg_op_code.get_or_insert(op_code);
+
+            if fin {
+                break;
+            }
+        }
+
+        let op_code = msg_op_code.expect("message op_code was empty");
+        match op_code {
+            OpCode::Binary => Ok(Message::Binary(msg_data)),
+            OpCode::Text => {
+                let text = String::from_utf8(msg_data)?;
+                Ok(Message::Text(text))
+            }
+            OpCode::Ping => Ok(Message::Ping(msg_data)),
+            OpCode::Pong => Ok(Message::Pong(msg_data)),
+            OpCode::Close => Ok(Message::Close),
+            OpCode::Continuation => {
+                unreachable!("Continuation must not be possible because all the data is aggregated")
+            }
+        }
+    }
 
     pub fn send(&mut self, message: Message) -> Result<(), BoxError> {
         todo!()
     }
 }
 
-#[derive(PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum OpCode {
     Continuation = 0x0,
     Text = 0x1,
@@ -146,8 +203,22 @@ impl OpCode {
             _ => None,
         }
     }
+
+    pub fn to_bit(&self) -> u8 {
+        *self as u8
+    }
 }
 
+#[derive(Debug, Clone)]
+pub enum Message {
+    Binary(Vec<u8>),
+    Text(String),
+    Ping(Vec<u8>),
+    Pong(Vec<u8>),
+    Close,
+}
+
+#[derive(Debug)]
 struct Frame {
     fin: bool,
     rsv1: u8,
@@ -160,11 +231,74 @@ struct Frame {
     payload: Vec<u8>,
 }
 
-#[derive(Debug, Clone)]
-pub enum Message {
-    Binary(Vec<u8>),
-    Text(String),
-    Ping(Vec<u8>),
-    Pong(Vec<u8>),
-    Close,
+impl Frame {
+    pub fn builder(op_code: OpCode) -> FrameBuilder {
+        FrameBuilder::new(op_code)
+    }
+
+    pub fn into_bytes(self) -> Vec<u8> {
+        // fin, rsv1, rsv2, rsv3, op_code
+        todo!()
+    }
+}
+
+#[derive(Debug)]
+struct FrameBuilder(Frame);
+impl FrameBuilder {
+    pub fn new(op_code: OpCode) -> Self {
+        FrameBuilder(Frame {
+            fin: true,
+            rsv1: 0,
+            rsv2: 0,
+            rsv3: 0,
+            op_code,
+            mask: false,
+            payload_len: 0,
+            masking_key: 0,
+            payload: Vec::new(),
+        })
+    }
+
+    pub fn fin(mut self, is_fin: bool) -> Self {
+        self.0.fin = is_fin;
+        self
+    }
+
+    pub fn rsv(mut self, rsv1: u8, rsv2: u8, rsv3: u8) -> Self {
+        self.0.rsv1 = rsv1;
+        self.0.rsv2 = rsv2;
+        self.0.rsv3 = rsv3;
+        self
+    }
+
+    pub fn mask(mut self, masking_key: Option<u32>) -> Self {
+        match masking_key {
+            Some(key) => {
+                self.0.mask = false;
+                self.0.masking_key = key;
+            }
+            None => {
+                self.0.mask = false;
+                self.0.masking_key = 0;
+            }
+        }
+
+        self
+    }
+
+    pub fn payload(mut self, payload: Vec<u8>) -> Self {
+        self.0.payload_len = payload.len() as u128;
+        self.0.payload = payload;
+        self
+    }
+
+    pub fn push_payload(mut self, payload: &[u8]) -> Self {
+        self.0.payload_len += payload.len() as u128;
+        self.0.payload.extend_from_slice(payload);
+        self
+    }
+
+    pub fn build(self) -> Frame {
+        self.0
+    }
 }
