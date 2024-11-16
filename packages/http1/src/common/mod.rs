18e4202d95f719c0d0c1c91e069f8bc6c9a6f491
Freddy L <neo-ciber_94@hotmail.com>
2024-11-16 01:50:00 -0400

    format

--- packages/http1/src/common/mod.rs
+++ packages/http1/src/common/mod.rs
@@ -1,8 +1,8 @@
 pub mod any_map;
+pub mod base64;
 pub mod find_open_port;
+pub mod sha1;
 pub mod temp_file;
 pub mod thread_pool;
 pub mod thread_spawner;
 pub mod uuid;
-pub mod base64;
-pub mod sha1;
