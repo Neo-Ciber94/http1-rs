3d345c9ecccb5ac2617c4a568690546417d257b2
Freddy L <neo-ciber_94@hotmail.com>
2024-11-24 00:57:52 -0400

    Add CloneableAnyMap

--- packages/http1/src/common/any_map.rs
+++ packages/http1/src/common/any_map.rs
@@ -4,11 +4,13 @@ use std::{
     fmt::Debug,
 };
 
+/// A map that holds data of any type.
+#[derive(Default)]
 pub struct AnyMap(HashMap<TypeId, Box<dyn Any + Send + Sync>>);
 
 impl AnyMap {
     pub fn new() -> Self {
-        AnyMap(Default::default())
+        Default::default()
     }
 
     pub fn len(&self) -> usize {
@@ -67,11 +69,11 @@ impl AnyMap {
     pub fn extend(&mut self, other: AnyMap) {
         self.0.extend(other.0);
     }
-}
 
-impl Default for AnyMap {
-    fn default() -> Self {
-        Self::new()
+    pub fn extend_from_cloneable(&mut self, other: CloneableAnyMap) {
+        for (key, value) in other.0 {
+            self.0.insert(key, value.into_any_box());
+        }
     }
 }
 
@@ -80,3 +82,113 @@ impl Debug for AnyMap {
         f.debug_tuple("AnyMap").finish()
     }
 }
+
+#[doc(hidden)]
+pub trait CloneableAny: Any + Send + Sync {
+    fn clone_box(&self) -> Box<dyn CloneableAny + Send + Sync>;
+    fn as_any(&self) -> &dyn Any;
+    fn as_any_mut(&mut self) -> &mut dyn Any;
+
+    fn into_any_box(self: Box<Self>) -> Box<dyn Any + Send + Sync>;
+}
+
+// impl dyn CloneableAny + Send + Sync {
+//     pub fn downcast_ref<T: Any>(&self) -> Option<&T> {
+//         self.as_any().downcast_ref::<T>()
+//     }
+
+//     pub fn downcast_mut<T: Any>(&mut self) -> Option<&mut T> {
+//         self.as_any_mut().downcast_mut::<T>()
+//     }
+// }
+
+impl<T> CloneableAny for T
+where
+    T: Any + Clone + Send + Sync,
+{
+    fn clone_box(&self) -> Box<dyn CloneableAny + Send + Sync> {
+        Box::new(self.clone())
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
+    fn as_any_mut(&mut self) -> &mut dyn Any {
+        self
+    }
+
+    fn into_any_box(self: Box<Self>) -> Box<dyn Any + Send + Sync> {
+        self
+    }
+}
+
+#[derive(Default)]
+pub struct CloneableAnyMap(HashMap<TypeId, Box<dyn CloneableAny + Send + Sync>>);
+
+impl CloneableAnyMap {
+    pub fn new() -> Self {
+        Default::default()
+    }
+
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
+
+    pub fn insert<T>(&mut self, value: T) -> Option<T>
+    where
+        T: Clone + Send + Sync + 'static,
+    {
+        self.0
+            .insert(TypeId::of::<T>(), Box::new(value))
+            .and_then(|x| x.into_any_box().downcast().ok())
+            .map(|x| *x)
+    }
+
+    pub fn contains<T>(&self) -> bool
+    where
+        T: Clone + Send + Sync + 'static,
+    {
+        self.0.contains_key(&TypeId::of::<T>())
+    }
+
+    // pub fn get<T>(&self) -> Option<T>
+    // where
+    //     T: Clone + Send + Sync + 'static,
+    // {
+    //     self.0
+    //         .get(&TypeId::of::<T>())
+    //         .map(|x| x.clone_box())
+    //         .and_then(|x| x.into_any().downcast().ok())
+    //         .map(|x| *x)
+    // }
+
+    pub fn remove<T>(&mut self) -> Option<T>
+    where
+        T: Clone + Send + Sync + 'static,
+    {
+        self.0
+            .remove(&TypeId::of::<T>())
+            .and_then(|x| x.into_any_box().downcast().ok())
+            .map(|x| *x)
+    }
+
+    pub fn extend(&mut self, other: CloneableAnyMap) {
+        self.0.extend(other.0);
+    }
+}
+
+// impl Clone for CloneableAnyMap {
+//     fn clone(&self) -> Self {
+//         let mut map = HashMap::with_capacity(self.len());
+//         for (k, v) in &self.0 {
+//             map.insert(*k, v.clone_box());
+//         }
+//         CloneableAnyMap(map)
+//     }
+// }
+
