3f13d7d1b11d66d3e081e734303411b59e00c375
Freddy L <neo-ciber_94@hotmail.com>
2024-12-04 21:59:31 -0400

    Update cors example

--- examples/cors/main.rs
+++ examples/cors/main.rs
@@ -1,5 +1,18 @@
-use http1::{method::Method, server::Server};
-use http1_web::{app::App, html::Html, middleware::cors::Cors};
+use std::sync::{Arc, Mutex};
+
+use http1::{body::Body, method::Method, server::Server};
+use http1_web::{
+    app::App,
+    forms::form::Form,
+    header::{GetHeader, Referer},
+    html::Html,
+    json::Json,
+    middleware::{cors::Cors, logging::Logging},
+    redirect::Redirect,
+    state::State,
+    HttpResponse, IntoHttpResponse,
+};
+use serde::impl_serde_struct;
 
 fn main() -> std::io::Result<()> {
     log::set_logger(log::ConsoleLogger);
@@ -12,6 +25,22 @@ fn main() -> std::io::Result<()> {
     Ok(())
 }
 
+#[derive(Debug, Clone)]
+struct Flower {
+    name: String,
+    color: String,
+}
+
+impl_serde_struct!(Flower => {
+    name: String,
+    color: String,
+});
+
+#[derive(Default, Clone, Debug)]
+struct AppState {
+    flowers: Arc<Mutex<Vec<Flower>>>,
+}
+
 fn backend() -> std::io::Result<()> {
     let app = App::new()
         .middleware(
@@ -20,8 +49,32 @@ fn backend() -> std::io::Result<()> {
                 .allow_origins(vec!["http://localhost:3456"])
                 .build(),
         )
-        .get("/api/users", || "All Users")
-        .post("/api/users", || "Create User");
+        .middleware(Logging)
+        .state(AppState::default())
+        .get("/api/flowers", |State(state): State<AppState>| {
+            let lock = state.flowers.lock().unwrap();
+            let flowers = (*lock).clone();
+            Json(flowers)
+        })
+        .post(
+            "/api/flowers",
+            |Form(flower): Form<Flower>,
+             State(state): State<AppState>,
+             referer: Option<GetHeader<Referer>>| {
+                dbg!(&flower);
+                let mut lock = state.flowers.lock().unwrap();
+                lock.push(flower);
+
+                match referer {
+                    Some(GetHeader(referer)) => {
+                        let s = referer.to_string();
+                        println!("{s}");
+                        Redirect::see_other(referer.to_string()).into_http_response()
+                    }
+                    None => HttpResponse::created(Body::empty()),
+                }
+            },
+        );
 
     Server::new()
         .on_ready(|addr| println!("Backend listening on http://localhost:{}", addr.port()))
@@ -34,28 +87,65 @@ fn frontend() -> std::io::Result<()> {
             r#"
         <html>
             <head>
-                <title>User List</title>
+                <title>🌹🪻 Flowers 🌻🌷</title>
+                <meta charset="utf-8">
+                <style>
+                    .flower-color {
+                        width: 10px;
+                        height: 10px;
+                        background-color: attr(data-color);
+                    }
+                </style>
                 <script>
                     window.onload = () => {
-                        const userDiv = document.getElementById('user-list');
+                        const flowersEl = document.getElementById('flower-list');
                         
-                        fetch('http://localhost:5000/api/users')
-                            .then(response => response.text()) 
-                            .then(text => {
-                                userDiv.textContent = text;
+                        fetch('http://localhost:5000/api/flowers')
+                            .then(res => res.json()) 
+                            .then(flowers => {
+
+                                if (flowers.length === 0) {
+                                   flowersEl.textContent = 'No flowers available 🥀'; 
+                                }
+                                else {
+                                    const listEl = document.createElement("ul");
+
+                                    for (const flower of flowers) {
+                                        const flowerEl = document.createElement("li");
+                                        const colorEl = document.createElement("span");
+                                        const textEl = document.createElement("span");
+
+                                        colorEl.classList.add("flower-color");
+                                        colorEl.dataset.color = flower.color;
+                                        textEl.textContent = flower.name;
+
+                                        flowerEl.append(colorEl);
+                                        flowerEl.append(textEl);
+                                    }
+
+                                    flowersEl.append(listEl);
+                                }
                             })
                             .catch(error => {
                                 console.error('Error fetching users:', error);
-                                userDiv.textContent = 'Failed to load users'; 
+                                flowersEl.textContent = '❌ Failed to load flowers'; 
                             });
                     };
                 </script>
             </head>
             <body>
-                <h1>Users</h1>
-                <div id="user-list">
+                <h1>🌸🌸 Flowers 🌸🌸</h1>
+                <div id="flower-list">
                    Loading...
                 </div>
+
+                <h2>New Flower</h2>
+                <form method="post" action="http://localhost:5000/api/flowers">
+                    <input name="name" type="text" required />
+                    <input name="color" type="color" required />
+                    <br/>
+                    <button>Add Flower</button>
+                </form>
             </body>
         </html>
         "#,
